6ffffa50df78779d64ced256b87dee601bab2d7d
Jeremy Pierce-Lord <118771875+jerpl@users.noreply.github.com>
2024-04-07 17:40:28 -0600

    Update page.jsx

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -6,7 +6,7 @@ const resume = () => {
   return (
     <div className='main'>
       <button type="button" onClick="alert('Hello world!')">Click Me!</button>
-      <a href="assets/files/resume.pdf" download={"/assets/files/resume.pdf"} target="_blank"> 
+      <a href="assets/files/resume.pdf" download={"assets/files/resume.pdf"} target="_blank"> 
         <button type='button'>hmm</button>
         </a>
       
